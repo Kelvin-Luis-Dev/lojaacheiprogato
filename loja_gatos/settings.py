d7400a53c3bea0eb692178b6b56feb204ea1106f
Kelvin Luis <parceriaskelvinluis@email.com>
2026-02-02 17:01:10 -0300

    Security: movendo SECRET_KEY para variáveis de ambiente

--- loja_gatos/settings.py
+++ loja_gatos/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-88fe2+ez@&9s2t717hsnc8irpzj1(#gmwf%u#7flcbt4nrp8wn'
-
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
